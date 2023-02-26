b3730a824b1b311b90b3253a74e9a23233a7e45e
jinny-l <jinny.l.dev@gmail.com>
2023-02-26 17:07:47 +0900

    ✨feat: BOJ_1051_숫자_정사각형 알고리즘 구현
    * 채점 실패, 이유를 모르겠음

---
+++ jinny-l/Week_01/BOJ_1051_숫자_정사각형.java
@@ -0,0 +1,43 @@
+package Week_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1051_숫자_정사각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] square = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String oneLine = br.readLine();
+            for (int j = 0; j < M; j++) {
+                square[i][j] = Character.getNumericValue(oneLine.charAt(j));
+            }
+        }
+
+        int len = Math.min(N, M);
+        int row = N - len;
+        int col = M - len;
+
+        loop:
+        while (len > 1) {
+            for (int i = 0; i <= row; i++) {
+                for (int j = 0; j <= col; j++) {
+                    int vertex = square[i][j];
+                    if (vertex == square[i][j + len - 1] && vertex == square[i + len - 1][j]
+                            && vertex == square[i + len - 1][j + len - 1]) {
+                        break loop;
+                    }
+                }
+            }
+            len--;
+        }
+        System.out.println(len * len);
+    }
+}
