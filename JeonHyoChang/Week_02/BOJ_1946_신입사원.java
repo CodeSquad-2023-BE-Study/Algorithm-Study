69a4555238647c6938332d7b7fe194594075facb
JeonHyoChang <jhc1426@naver.com>
2023-03-01 17:13:47 +0900

    ✏️comment: 1946_신입사원
    1차 등수로 정렬시 첫번째 등수의 사람은 다른사람보다 무조건 높기에 합격자 이다.
    1차 등수는 정렬되어있어서 2차 등수를 비교를 해야하는데, 현재 합격자의 2차 등수와 순차적으로 비교하며, 합격할시 이와같은 흐름을 반복한다.

--- JeonHyoChang/Week_02/BOJ_1946_신입사원.java
+++ JeonHyoChang/Week_02/BOJ_1946_신입사원.java
@@ -32,13 +32,16 @@ public class BOJ_1946_신입사원 {
                 grade_List.add(new Grade(fg, sg));
             }
 
+            // 기존 방식에서 list.sort()로 변경
             grade_List.sort((o1, o2) -> {
                 return o1.fg - o2.fg;
             });
 
+            // 합격자 수 & 기준이 되는 2차 성적
             int passer_Count = 1;
             int standard = grade_List.get(0).sg;
 
+            // 기준보다 등수가 높을시 합격자에 추가하며, 기준을 그사람으로 변경
             for (int x = 1; x < N; x++) {
                 if (standard > grade_List.get(x).sg) {
                     passer_Count++;
