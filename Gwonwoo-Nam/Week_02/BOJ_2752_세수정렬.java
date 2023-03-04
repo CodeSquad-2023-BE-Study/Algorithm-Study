e4c69b1ef297179dcbb2b38b926e503c42e7e5ac
Gwonwoo-Nam <ngw7617@naver.com>
2023-03-04 14:16:06 +0900

    ✏️comment:[BOJ]2752.세수정렬 주석 및 문제 풀이 작성

--- Gwonwoo-Nam/Week_02/BOJ_2752_세수정렬.java
+++ Gwonwoo-Nam/Week_02/BOJ_2752_세수정렬.java
@@ -6,6 +6,12 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BOJ_2752_세수정렬 {
+
+    /**
+     * 문제 풀이
+     * 1. 입력을 int의 배열에 저장
+     * 2. Arrays.sort로 오름차순 정렬 후 출력
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
