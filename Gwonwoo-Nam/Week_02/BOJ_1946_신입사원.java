7e64ec7652a76056b93da153ce85ff29ea7fc978
Gwonwoo-Nam <ngw7617@naver.com>
2023-03-04 14:16:43 +0900

    ✏️comment:[BOJ]1946.신입사원 주석 및 문제 풀이 작성

--- Gwonwoo-Nam/Week_02/BOJ_1946_신입사원.java
+++ Gwonwoo-Nam/Week_02/BOJ_1946_신입사원.java
@@ -6,6 +6,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BOJ_1946_신입사원 {
+    /**
+     * 문제 풀이
+     *
+     * 한 점수로 오름차순 정렬을 하면,
+     * 다른 점수는 반드시 앞 사람보다 등수가 낮아야 합격할 수 있습니다.
+     * 따라서 두번째 시험 성적의 최소 값을 가지는 변수를 선언하여, 최소 값이 갱신될 때마다 count를 증가시켜줍니다.
+     *
+     * 예시)
+     * 1 4 - O
+     * 2 5 - X
+     * 3 6 - X
+     * 4 2 - O
+     * 5 7 - X
+     * 6 1 - O
+     * 7 3 - X
+     *
+     * 문제 풀이
+     * 1. 등수 a,b를 가지는 Person Class
+     * 2. compareTo를 override하여 a의 등수로 오름차순 정렬하도록 구현
+     * 3. person list를 정렬한 후, 순회하면서 최소 값이 갱신될 때마다 count를 증가시킨다.
+     * 4. count를 출력
+     */
 
     static class Person implements Comparable {
 
