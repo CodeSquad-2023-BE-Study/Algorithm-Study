f7d2007d06ea8386b81de31cc1e08a6b56f46d79
Gwonwoo-Nam <ngw7617@naver.com>
2023-03-18 02:22:26 +0900

    ✨feat: [BOJ] 3273_두수의합 문제 구현
    - 주몽 코드를 복붙하니 통과하더군요.

--- Gwonwoo-Nam/week_04/BOJ_3273_두수의합.java
+++ Gwonwoo-Nam/week_04/BOJ_3273_두수의합.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class BOJ_3273_두수의합 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_3273_두수의합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+
+        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
+            .toArray();
+        int m = Integer.parseInt(br.readLine());
+
+        Arrays.sort(array);
+
+        int i=0;
+        int j=0;
+        int count=0;
+
+        while (j < n-1 && array[j] < m) {
+            j++;
+        }
+        while (i<j) {
+            if (array[i] + array[j] == m) {
+                count++;
+                i++;
+                j--;
+            }
+            else if (array[i] + array[j] > m) {
+                j--;
+            }
+            else if (array[i] + array[j] < m) {
+                i++;
+            }
+        }
+
+        System.out.println(count);
+    }
 }
