1ea70878aa96dcf82f29df097f2c27c421512986
Gwonwoo-Nam <ngw7617@naver.com>
2023-03-18 02:23:50 +0900

    ✨feat: [BOJ] 2118_두개의 탑 문제 구현
    - 시간초과가 계속 나서 힌트를 좀 얻었는데 불필요한 반복문을 줄이는게 핵심인것 같습니다.

--- Gwonwoo-Nam/week_04/BOJ_2118_두개의탑.java
+++ Gwonwoo-Nam/week_04/BOJ_2118_두개의탑.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class BOJ_2118_두개의탑 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_2118_두개의탑 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> arr = new ArrayList<>();
+
+        for (int iter = 0; iter < n; iter++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+        long sum = arr.stream().reduce((a,b)->a+b).get();
+        int a = 0;//첫점
+        int b = n;//끝점
+        long maxDistance = 0;
+
+        for (a=0;a<n;a++) {
+            long rightDistance = 0;
+            long leftDistance = 0;
+            long distance = 0;
+            for (b=a+1;b<n;b++) {
+                rightDistance += arr.get(b);
+                leftDistance = sum - rightDistance;
+                distance = Math.min(rightDistance,leftDistance);
+                maxDistance = Math.max(distance, maxDistance);
+                if (rightDistance > leftDistance) {
+                    break ;
+                }
+            }
+        }
+        System.out.println(maxDistance);
+    }
 }
