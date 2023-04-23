40c4ce20e4f02be94053a3df53e44ae83d213c11
JeeIn Lee <jinny.l.dev@gmail.com>
2023-04-23 14:34:02 +0900

    [Week 08] BOJ DP 알고리즘 - 2문제 완료 (#49)
    🔀merge: [Week 08] BOJ DP 알고리즘 - 2문제 완료

---
+++ jinny-l/week_08/BOJ_11053_가장_긴_증가하는_부분_수열.java
@@ -0,0 +1,48 @@
+package week_08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11053_가장_긴_증가하는_부분_수열 {
+
+    private static int[] sequence;
+    private static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력: 수열 길이
+        int length = Integer.parseInt(br.readLine());
+
+        // 배열 초기화
+        sequence = new int[length];
+        dp = new int[length];
+
+        // 입력: 수열
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < length; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 출력
+        System.out.println(lis());
+    }
+
+    private static int lis() {
+        int max = 0;
+
+        for (int i = 0; i < sequence.length; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (sequence[i] > sequence[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
