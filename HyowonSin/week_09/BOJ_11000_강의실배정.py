3e386a4a5d4337ec395ca6e6c28d64208d8326d9
HyowonSin <tlsgydnjs@naver.com>
2023-04-25 23:18:41 +0900

    🐛fix: BOJ_11000_강의실배정 문제 시간 초과 해결
    사용 중인 강의실을 리스트로 관리하고,
    매 번 정렬을 다시 해주니 시간 초과가 났습니다.
    
    우선 순위 큐를 활용하여 값을 넣으면서
    정렬을 하도록 했습니다.
    
    유형: 자료 구조, 그리디 알고리즘, 정렬, 우선순위 큐
    난이도: 골드 5

--- HyowonSin/week_09/BOJ_11000_강의실배정.py
+++ HyowonSin/week_09/BOJ_11000_강의실배정.py
@@ -1,4 +1,5 @@
 import sys
+from queue import PriorityQueue
 
 N = int(sys.stdin.readline())
 
@@ -7,16 +8,16 @@ I = []
 for i in range(N):
     I.append(list(map(int, sys.stdin.readline().split())))
 
-I.sort(key=lambda x: (x[1], x[0]))
+I.sort(key=lambda x: (x[0], x[1]))
 
-room = [I[0][1]]
+room = PriorityQueue()
+room.put(I[0][1])
 
 for i in I[1:]:
-    for j in room:
-        if i[0] >= j:
-            room[room.index(j)] = i[1]
-            break
+    if room.queue[0] <= i[0]:
+        room.get()
+        room.put(i[1])
     else:
-        room.append(i[1])
+        room.put(i[1])
 
-print(len(room))
+print(room.qsize())
