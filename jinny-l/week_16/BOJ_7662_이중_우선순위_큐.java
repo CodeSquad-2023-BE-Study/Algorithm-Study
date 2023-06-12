b0a60d46e49d4507a0bcda13c9be230d23b3e4cd
JeeIn Lee <jinny.l.dev@gmail.com>
2023-06-13 01:30:37 +0900

    🔀merge: [Week 16] BOJ 자료구조 2 - 3문제 완료 (#69)
    * ✨feat: BOJ_2075_N번째_큰_수 알고리즘 구현
    
    * 처음에 ArrayList로 풀었는데 시간초과가 안나서 우선순위 큐로도 한번 더 풀었다.
    
    * ✨feat: BOJ_7662_이중_우선순위_큐 알고리즘 구현
    
    * 최대 힙, 최소 힙을 구현한 두개의 우선순위 큐로 풀면 remove() 메서드로 인해 시간초과가 난다.
    * TreeMap은 흑적나무로 구현되어 있어서 양쪽 끝단의 숫자를 O(1)로 꺼내올 수 있다.
    
    * ✨feat: BOJ_1620_나는야_포켓몬_마스터_이다솜 알고리즘 구현
    
    * 다른 풀이로는 두개의 HashMap에 [이름, 인덱스], [인덱스, 이름]을 저장하는 방법이 있겠다.
    * 또 다른 풀이로는 한개의 HashMap에 [이름, 인덱스], [인덱스, 이름]을 문자열 형태로 두 번 저장하는 방법이 있겠다.
    * 숫자 or 문자열 여부를 판단하는 방법이 조금 고민되었다.

---
+++ jinny-l/week_16/BOJ_7662_이중_우선순위_큐.java
@@ -0,0 +1,65 @@
+package week_16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+// PriorityQueue를 사용해서 두개의 큐를 사용해서 풀이 시, remove로 인해 시간초과 발생
+// TreeMap은 흑적나무로 구현되어 있어서 양쪽 끝단의 숫자를 O(1)로 꺼내올 수 있다.
+public class BOJ_7662_이중_우선순위_큐 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 테케 입력
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < tc; i++) { // 테케만큼 반복
+
+            // 개수 입력
+            int n = Integer.parseInt(br.readLine());
+
+            // 트리맵으로 저장
+            TreeMap<Integer, Integer> map = new TreeMap<>(); // Key: 숫자, Value: 개수
+
+            for (int j = 0; j < n; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                String operator = st.nextToken();
+                int number = Integer.parseInt(st.nextToken());
+
+                if (operator.equals("I")) {
+                    map.put(number, map.getOrDefault(number, 0) + 1);
+                    continue;
+                }
+
+                if (operator.equals("D")) {
+                    if (map.size() < 1) { // 맵이 비어있으면 무시
+                        continue;
+                    }
+
+                    // 맵이 비어있지 않다면 min or max 값 찾기
+                    int toDelete = number == 1 ? map.lastKey() : map.firstKey();
+                    int value = map.get(toDelete);
+
+                    if (value == 1) { // 삭제했을 때 값이 0이면 삭제
+                        map.remove(toDelete);
+                    } else { // 값이 0이 아니면 value -1
+                        map.put(toDelete, map.getOrDefault(toDelete, 0) - 1);
+                    }
+                }
+            }
+            if (map.size() < 1) {
+                sb.append("EMPTY\n");
+            } else {
+                sb.append(map.lastKey())
+                        .append(" ")
+                        .append(map.firstKey())
+                        .append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
