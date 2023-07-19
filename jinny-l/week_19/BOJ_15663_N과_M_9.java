e64d8392bbd995d556c26c6b7f680b06b9d94e24
jinny-l <jinny.l.dev@gmail.com>
2023-07-19 19:37:54 +0900

    ✨feat: 20주차 백트래킹 문제 풀이

---
+++ jinny-l/week_19/BOJ_15663_N과_M_9.java
@@ -0,0 +1,61 @@
+package week_19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_15663_N과_M_9 {
+
+    private static int[] arr;
+    private static int[] tmp;
+    private static boolean[] visited;
+    private static Set<String> set;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // 입력
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        // 풀이
+        tmp = new int[m];
+        visited = new boolean[n];
+        set = new LinkedHashSet<>();
+        dfs(n, m, 0);
+        set.forEach(System.out::println);
+    }
+
+    private static void dfs(int n, int m, int depth) {
+        if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < tmp.length; i++) {
+                sb.append(tmp[i]).append(" ");
+            }
+            set.add(sb.toString());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                tmp[depth] = arr[i];
+                dfs(n, m, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
