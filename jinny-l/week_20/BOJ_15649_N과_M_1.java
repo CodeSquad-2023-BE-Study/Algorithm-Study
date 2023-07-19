90b603034ad0c9e978fb67167cdf47779b57c9c6
JeeIn Lee <jinny.l.dev@gmail.com>
2023-07-19 19:55:04 +0900

    🔀merge: [Week20~21] BOJ Backtracking + Shortest Path - 2문제 + 1문제 완료 (#76)
    * ✨feat: 20주차 백트래킹 문제 풀이
    
    * ✨feat: BOJ_11403_경로_찾기 문제 풀이
    
    * 플로이드 워샬 알고리즘 활용
    
    * 🚚rename: 패키지 이름 수정

---
+++ jinny-l/week_20/BOJ_15649_N과_M_1.java
@@ -0,0 +1,50 @@
+package week_20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15649_N과_M_1 {
+
+    private static boolean[] visited;
+    private static int[] tmp;
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // 개수 입력
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        // 풀이
+        visited = new boolean[n];
+        tmp = new int[m];
+        sb = new StringBuilder();
+
+        dfs(n, m, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int n, int m, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < tmp.length; i++) {
+                sb.append(tmp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                tmp[depth] = i + 1;
+                dfs(n, m, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
